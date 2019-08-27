aec8cd2bb6b1aea8c22d74e020e7b5ca846a0d7e
Hedesil <mariano25294@gmail.com>
2019-08-27 20:20:42 +0200

    Try to connect with a device using async/await nomenclature.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,48 @@ import {Component} from '@angular/core';
 
 
 export class AppComponent {
-  title = 'carpcOBD';
-  errorMessage: string;
+  title = 'CarPC';
+  Message: string;
+
   constructor() {
 
   }
 
-  public connect() {
-    this.errorMessage = 'Esperando error...';
-    navigator.bluetooth.requestDevice({acceptAllDevices: true})
-      .then(device => {
-        let deviceInfo: any;
-        deviceInfo.name = device.name;
-        deviceInfo.id = device.id;
-        console.log(device);
-        console.log('User paired with device name:', device.name,
-          'id:', device.id);
-      })
-      .catch(error => {
-        let errMes = error.message;
-        this.errorMessage = errMes;
-        console.log(errMes)
+  async onButtonClick() {
+    try {
+      console.log('Requesting Bluetooth Device...');
+      const device = await navigator.bluetooth.requestDevice({
+        acceptAllDevices: true
       });
+
+      const deviceInfo: any = {
+        name: device ? device.name : device.id,
+        id: device.id
+      };
+
+      if (deviceInfo.name !== null && device.name !== undefined) {
+        this.Message = 'Has encontrado ' + deviceInfo.name;
+      } else {
+        this.Message = 'Has encontrado ' + deviceInfo.id;
+        console.log(device);
+      }
+
+      console.log('Connecting to GATT Server...');
+      const server = await device.gatt.connect();
+
+      console.log('Getting Battery Service...');
+      const service = await server.getPrimaryService('battery_service');
+
+      console.log('Getting Battery Level Characteristic...');
+      const characteristic = await service.getCharacteristic('battery_level');
+
+      console.log('Reading Battery Level...');
+      const value = await characteristic.readValue();
+
+      console.log('> Battery Level is ' + value.getUint8(0) + '%');
+    } catch (error) {
+      console.log('Argh! ' + error);
+    }
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AppComponent} from './app.component';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 import {WebBluetoothModule} from '@manekinekko/angular-web-bluetooth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {WebBluetoothModule} from '@manekinekko/angular-web-bluetooth';
     ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
     WebBluetoothModule.forRoot({
       enableTracing: true // or false, this will enable logs in the browser's console
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
