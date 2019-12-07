3be0d1b24616680c51b33aca8dbb66697d360e01
Mariano Moreno <mariano25294@gmail.com>
2019-12-07 16:09:03 +0100

    Added material styles to component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,4 +61,6 @@ export class AppComponent {
     }
   }
 
+  show
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,24 @@ import {AppComponent} from './app.component';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 import {WebBluetoothModule} from '@manekinekko/angular-web-bluetooth';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MatButtonToggleModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
     WebBluetoothModule.forRoot({
       enableTracing: true // or false, this will enable logs in the browser's console
     }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
